31dfa20b8a6176793ab3a3f294a0660614600535
mushroom <nazarzhebrun@gmail.com>
2023-10-05 10:25:46 +0300

    simplest tasks
    pyramid build

---
+++ simplest problem pyramid.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+ 
+    int a;
+    string sym;
+    cout << "\n   Ener a symbol from which the pyramid will be made:";
+    cin >> sym;
+    cout << "\n   Ener a height of the pyramid:";
+    cin >> a;
+  
+   for (int i = 0; i < a; i++)
+    {
+        for (int j = a; j >= 0; j--)
+        {
+            if (j <= i) 
+            cout << sym;
+            cout << ' ';
+        }
+        cout <<endl;
+    }
+}
